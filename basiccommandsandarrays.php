cb4980f8fa1b2eea895324f249811c1fff03d93d
benhvail <79860384+benhvail@users.noreply.github.com>
2021-12-09 11:18:15 -0500

    Add files via upload
    mvp

---
+++ basiccommandsandarrays.php
@@ -0,0 +1,15 @@
+<?php
+$MyFirstVariable = 'Ben';
+echo 'My first variable was named $MyFirstVariable'. '<br />';
+ echo 'The value of my variable was '. $MyFirstVariable . '<br />';
+$empty = array ();
+$num5 = array($firstname = 'Ben', $lastname = 'Vail', $semester = 3);
+$numbers = array ();
+for ($x = 0; $x <50; $x++) {
+    $numbers[$x]=$x+1;
+
+
+}
+echo mb_strlen($MyFirstVariable);
+    
+?>
